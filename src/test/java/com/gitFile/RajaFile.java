e6a4fb10c00428d25c2ff69280b9caa6dae7e514
Raja-Manikandan <rajarj017@gmail.com>
2024-09-13 12:35:06 +0530

    Hicodeadded

---
+++ src/test/java/com/gitFile/RajaFile.java
@@ -0,0 +1,15 @@
+package com.gitFile;
+
+public class RajaFile {
+	
+ public void test1() {
+System.out.println("123");
+ }
+ public void test2() {
+System.out.println("Raja");
+ }
+ public void test3() {
+System.out.println("git123");
+ }
+
+}
